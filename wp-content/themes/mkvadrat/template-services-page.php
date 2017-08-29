e7e2753df327149ff62cc4bfcb0946e48acc7707
Середа Дмитрий <q3dm0@yahoo.com>
2017-08-29 17:42:09 +0300

    Готова страница Услуги

---
+++ wp-content/themes/mkvadrat/template-services-page.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Template name: Services page
+Theme Name: Mkvadrat
+Theme URI: http://mkvadrat.com/
+Author: M2
+Author URI: http://mkvadrat.com/
+Description: Тема для сайта http://mkvadrat.com/
+Version: 1.0
+*/
+
+get_header(); 
+?>
+
+    <!-- start main-services -->
+
+    <main class="main-services">
+
+        <div class="container-fluid block-under-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'title_text_first_block_services_page', $single = true )); ?>
+                    </div>
+                    <div class="col-md-6"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid bg-blue">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="two-halves-block">
+                            <div class="right-side">
+                                <div class="empty-block-white bottom-left"></div>
+                                <div class="bottom-right light-gray">
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'services_a_second_block_services_page', $single = true )); ?>
+                                </div>
+                            </div>
+                            <div class="left-side">
+                                <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'title_text_second_block_services_page', $single = true )); ?>
+                            </div>
+                        </div>
+
+                        <div class="two-halves-block">
+                            <div class="right-side">
+                                <div class="left-top light-gray">
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'services_b_second_block_services_page', $single = true )); ?>
+                                </div>
+                                <div class="top-right dark-gray">
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'services_c_second_block_services_page', $single = true )); ?>
+                                </div>
+                                <div class="empty-block-white-2 bottom-left rotate"></div>
+                                <div class="bottom-right light-gray">
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'services_d_second_block_services_page', $single = true )); ?>
+                                </div>
+                            </div>
+                            <div class="left-side bottom-line">
+                                <div class="left-top light-gray">
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'services_e_second_block_services_page', $single = true )); ?>
+                                </div>
+                                <div class="top-right dark-gray">
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'services_f_second_block_services_page', $single = true )); ?>
+                                </div>
+                                    <?php echo html_entity_decode( get_post_meta( get_the_ID(), 'info_text_second_block_services_page', $single = true )); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid want-order">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="title">хотите заказать услугу?</p>
+                        <div class="order-block">
+                            <input type="text" id="email" placeholder="Введите Ваш e-mail">
+                            <input type="submit" onclick="SendShort();" value="Заказать">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <!-- end main-services -->
+    
+<script type="text/javascript">
+//форма обратной связи
+function SendShort() {
+	var data = {
+		'action': 'SendShort',
+		'email' : $('#email').val(),
+	};
+	$.ajax({
+		url:'http://' + location.host + '/wp-admin/admin-ajax.php',
+		data:data,
+		type:'POST',
+		success:function(data){
+			swal(data.message);
+		}
+	});
+};
+</script>
+  
+<?php get_footer(); ?>
